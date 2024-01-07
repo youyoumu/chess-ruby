931665361b3068307bc2399497d3a2fee5274c8d
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-01-07 16:31:50 +0700

    Complete #generate_move for Rook class

--- lib/rook.rb
+++ lib/rook.rb
@@ -1,4 +1,7 @@
+require './lib/chessman.rb'
+
 class Rook
+  include Chessman
   attr_accessor :coord, :color, :icon, :is_captured, :name
   def initialize(coord, color)
     @coord = coord
@@ -6,5 +9,58 @@ class Rook
     @icon = color ? " ♖ " : " ♜ "
     @is_captured = false
     @name = 'Rook'
+  end
+
+  def move(coord)
+    @coord = coord
+  end
+
+  def generate_move(board_obj)
+    generate_move_up(board_obj) + generate_move_down(board_obj) +
+    generate_move_left(board_obj) + generate_move_right(board_obj)
+  end
+
+  def generate_move_up(board_obj)
+    arr = []
+    move_coord = @coord
+    board_obj[move_coord[0]][move_coord[1]] = nil
+    while in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+      move_coord = [move_coord[0] - 1, move_coord[1]]
+      arr << move_coord if in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+    end
+    arr
+  end
+
+  def generate_move_down(board_obj)
+    arr = []
+    move_coord = @coord
+    board_obj[move_coord[0]][move_coord[1]] = nil
+    while in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+      move_coord = [move_coord[0] + 1, move_coord[1]]
+      arr << move_coord if in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+    end
+    arr
+  end
+
+  def generate_move_left(board_obj)
+    arr = []
+    move_coord = @coord
+    board_obj[move_coord[0]][move_coord[1]] = nil
+    while in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+      move_coord = [move_coord[0], move_coord[1] - 1]
+      arr << move_coord if in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+    end
+    arr
+  end
+
+  def generate_move_right(board_obj)
+    arr = []
+    move_coord = @coord
+    board_obj[move_coord[0]][move_coord[1]] = nil
+    while in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+      move_coord = [move_coord[0], move_coord[1] + 1]
+      arr << move_coord if in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+    end
+    arr
   end
 end
---
+++ spec/rook_spec.rb
@@ -0,0 +1,38 @@
+require './lib/board.rb'
+
+describe Rook do
+  subject(:game) { Board.new('Player 1', 'Player 2') }
+  let(:obj) { game.instance_variable_get(:@board_obj) }
+  let(:playerw) { game.instance_variable_get(:@playerw) }
+  let(:playerb) { game.instance_variable_get(:@playerb) }
+  let(:rookw1) { playerw.instance_variable_get(:@rook1) }
+  let(:pawnw1) { playerw.instance_variable_get(:@pawn1) }
+
+  describe "#generate_move" do
+    context "when setup #1" do
+      before do
+        game.update
+        pawnw1.move([4, 0])
+        game.update
+        game.draw_board
+      end
+      it "generate correct move for setup #1" do
+        expect(rookw1.generate_move(obj)).to match_array([[5, 0], [6, 0]])
+      end
+    end
+
+    context "when setup #2" do
+      before do
+        game.update
+        pawnw1.move([4, 0])
+        game.update
+        rookw1.move([5, 0])
+        game.update
+        game.draw_board
+      end
+      it "generate correct move for setup #2" do
+        expect(rookw1.generate_move(obj)).to match_array([[5, 1], [5, 2], [5, 3], [5, 4], [5, 5], [5, 6], [5, 7], [6, 0], [7, 0]])
+      end
+    end
+  end
+end

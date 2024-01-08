e34da0fb0ed97e08c4b81465694dc04d18d6e011
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-01-08 14:32:45 +0700

    Make King class playable

--- lib/king.rb
+++ lib/king.rb
@@ -10,4 +10,79 @@ class King
     @is_captured = false
     @name = 'King'
   end
+
+  def move(coord)
+    @coord = coord
+  end
+
+  def capture(coord, board_obj)
+    board_obj[coord[0]][coord[1]].captured
+    @coord = coord
+  end
+
+  def take_turn(coord, board_obj)
+    arr_move = generate_move(board_obj)
+    arr_move.include?(coord) ? move(coord) : capture(coord, board_obj)
+  end
+
+  def generate_move(board_obj)
+    arr = []
+    coord = [@coord[0] - 1, @coord[1] - 1]
+    arr << coord if in_bound?(coord) && board_obj[coord[0]][coord[1]].nil?
+    coord = [@coord[0] - 1, @coord[1] + 1]
+    arr << coord if in_bound?(coord) && board_obj[coord[0]][coord[1]].nil?
+    coord = [@coord[0] + 1, @coord[1] - 1]
+    arr << coord if in_bound?(coord) && board_obj[coord[0]][coord[1]].nil?
+    coord = [@coord[0] + 1, @coord[1] + 1]
+    arr << coord if in_bound?(coord) && board_obj[coord[0]][coord[1]].nil?
+    coord = [@coord[0] - 1, @coord[1]]
+    arr << coord if in_bound?(coord) && board_obj[coord[0]][coord[1]].nil?
+    coord = [@coord[0], @coord[1] - 1]
+    arr << coord if in_bound?(coord) && board_obj[coord[0]][coord[1]].nil?
+    coord = [@coord[0] + 1, @coord[1]]
+    arr << coord if in_bound?(coord) && board_obj[coord[0]][coord[1]].nil?
+    coord = [@coord[0], @coord[1] + 1]
+    arr << coord if in_bound?(coord) && board_obj[coord[0]][coord[1]].nil?
+    arr
+  end
+
+  def generate_capture(board_obj)
+    arr = []
+    if @color
+      coord = [@coord[0] - 1, @coord[1] - 1]
+      arr << coord if in_bound?(coord) && !board_obj[coord[0]][coord[1]].nil? && !board_obj[coord[0]][coord[1]].color
+      coord = [@coord[0] - 1, @coord[1] + 1]
+      arr << coord if in_bound?(coord) && !board_obj[coord[0]][coord[1]].nil? && !board_obj[coord[0]][coord[1]].color
+      coord = [@coord[0] + 1, @coord[1] - 1]
+      arr << coord if in_bound?(coord) && !board_obj[coord[0]][coord[1]].nil? && !board_obj[coord[0]][coord[1]].color
+      coord = [@coord[0] + 1, @coord[1] + 1]
+      arr << coord if in_bound?(coord) && !board_obj[coord[0]][coord[1]].nil? && !board_obj[coord[0]][coord[1]].color
+      coord = [@coord[0] - 1, @coord[1]]
+      arr << coord if in_bound?(coord) && !board_obj[coord[0]][coord[1]].nil? && !board_obj[coord[0]][coord[1]].color
+      coord = [@coord[0], @coord[1] - 1]
+      arr << coord if in_bound?(coord) && !board_obj[coord[0]][coord[1]].nil? && !board_obj[coord[0]][coord[1]].color
+      coord = [@coord[0] + 1, @coord[1]]
+      arr << coord if in_bound?(coord) && !board_obj[coord[0]][coord[1]].nil? && !board_obj[coord[0]][coord[1]].color
+      coord = [@coord[0], @coord[1] + 1]
+      arr << coord if in_bound?(coord) && !board_obj[coord[0]][coord[1]].nil? && !board_obj[coord[0]][coord[1]].color
+    else
+      coord = [@coord[0] - 1, @coord[1] - 1]
+      arr << coord if in_bound?(coord) && !board_obj[coord[0]][coord[1]].nil? && board_obj[coord[0]][coord[1]].color
+      coord = [@coord[0] - 1, @coord[1] + 1]
+      arr << coord if in_bound?(coord) && !board_obj[coord[0]][coord[1]].nil? && board_obj[coord[0]][coord[1]].color
+      coord = [@coord[0] + 1, @coord[1] - 1]
+      arr << coord if in_bound?(coord) && !board_obj[coord[0]][coord[1]].nil? && board_obj[coord[0]][coord[1]].color
+      coord = [@coord[0] + 1, @coord[1] + 1]
+      arr << coord if in_bound?(coord) && !board_obj[coord[0]][coord[1]].nil? && board_obj[coord[0]][coord[1]].color
+      coord = [@coord[0] - 1, @coord[1]]
+      arr << coord if in_bound?(coord) && !board_obj[coord[0]][coord[1]].nil? && board_obj[coord[0]][coord[1]].color
+      coord = [@coord[0], @coord[1] - 1]
+      arr << coord if in_bound?(coord) && !board_obj[coord[0]][coord[1]].nil? && board_obj[coord[0]][coord[1]].color
+      coord = [@coord[0] + 1, @coord[1]]
+      arr << coord if in_bound?(coord) && !board_obj[coord[0]][coord[1]].nil? && board_obj[coord[0]][coord[1]].color
+      coord = [@coord[0], @coord[1] + 1]
+      arr << coord if in_bound?(coord) && !board_obj[coord[0]][coord[1]].nil? && board_obj[coord[0]][coord[1]].color
+    end
+    arr
+  end
 end

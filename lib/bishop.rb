295cdab1124475120f3098fa9b94c5a2700ba1b3
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-01-08 13:14:26 +0700

    Make Bishop class playable

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -10,4 +10,148 @@ class Bishop
     @is_captured = false
     @name = 'Bishop'
   end
+
+  def move(coord)
+    @coord = coord
+  end
+
+  def capture(coord, board_obj)
+    board_obj[coord[0]][coord[1]].captured
+    @coord = coord
+  end
+
+  def take_turn(coord, board_obj)
+    arr_move = generate_move(board_obj)
+    arr_move.include?(coord) ? move(coord) : capture(coord, board_obj)
+  end
+
+  def generate_move(board_obj)
+    generate_move_up_left(board_obj) +
+    generate_move_up_right(board_obj) +
+    generate_move_down_left(board_obj) +
+    generate_move_down_right(board_obj)
+  end
+
+  def generate_capture(board_obj)
+    generate_capture_up_left(board_obj) +
+    generate_capture_up_right(board_obj) +
+    generate_capture_down_left(board_obj) +
+    generate_capture_down_right(board_obj)
+  end
+
+  def generate_move_up_left(board_obj)
+    arr = []
+    move_coord = @coord
+    board_obj[move_coord[0]][move_coord[1]] = nil
+    while in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+      move_coord = [move_coord[0] - 1, move_coord[1] - 1]
+      arr << move_coord if in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+    end
+    arr
+  end
+
+  def generate_move_up_right(board_obj)
+    arr = []
+    move_coord = @coord
+    board_obj[move_coord[0]][move_coord[1]] = nil
+    while in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+      move_coord = [move_coord[0] - 1, move_coord[1] + 1]
+      arr << move_coord if in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+    end
+    arr
+  end
+
+  def generate_move_down_left(board_obj)
+    arr = []
+    move_coord = @coord
+    board_obj[move_coord[0]][move_coord[1]] = nil
+    while in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+      move_coord = [move_coord[0] + 1, move_coord[1] - 1]
+      arr << move_coord if in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+    end
+    arr
+  end
+
+  def generate_move_down_right(board_obj)
+    arr = []
+    move_coord = @coord
+    board_obj[move_coord[0]][move_coord[1]] = nil
+    while in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+      move_coord = [move_coord[0] + 1, move_coord[1] + 1]
+      arr << move_coord if in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+    end
+    arr
+  end
+
+  def generate_capture_up_left(board_obj)
+    arr = []
+    move_coord = @coord
+    board_obj[move_coord[0]][move_coord[1]] = nil
+    if @color
+      while in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+        move_coord = [move_coord[0] - 1, move_coord[1] - 1]
+        arr << move_coord if in_bound?(move_coord) && !board_obj[move_coord[0]][move_coord[1]].nil? && !board_obj[move_coord[0]][move_coord[1]].color
+      end
+    else
+      while in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+        move_coord = [move_coord[0] - 1, move_coord[1] - 1]
+        arr << move_coord if in_bound?(move_coord) && !board_obj[move_coord[0]][move_coord[1]].nil? && board_obj[move_coord[0]][move_coord[1]].color
+      end
+    end
+    arr
+  end
+
+  def generate_capture_up_right(board_obj)
+    arr = []
+    move_coord = @coord
+    board_obj[move_coord[0]][move_coord[1]] = nil
+    if @color
+      while in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+        move_coord = [move_coord[0] - 1, move_coord[1] + 1]
+        arr << move_coord if in_bound?(move_coord) && !board_obj[move_coord[0]][move_coord[1]].nil? && !board_obj[move_coord[0]][move_coord[1]].color
+      end
+    else
+      while in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+        move_coord = [move_coord[0] - 1, move_coord[1] + 1]
+        arr << move_coord if in_bound?(move_coord) && !board_obj[move_coord[0]][move_coord[1]].nil? && board_obj[move_coord[0]][move_coord[1]].color
+      end
+    end
+    arr
+  end
+
+  def generate_capture_down_left(board_obj)
+    arr = []
+    move_coord = @coord
+    board_obj[move_coord[0]][move_coord[1]] = nil
+    if @color
+      while in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+        move_coord = [move_coord[0] + 1, move_coord[1] - 1]
+        arr << move_coord if in_bound?(move_coord) && !board_obj[move_coord[0]][move_coord[1]].nil? && !board_obj[move_coord[0]][move_coord[1]].color
+      end
+    else
+      while in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+        move_coord = [move_coord[0] + 1, move_coord[1] - 1]
+        arr << move_coord if in_bound?(move_coord) && !board_obj[move_coord[0]][move_coord[1]].nil? && board_obj[move_coord[0]][move_coord[1]].color
+      end
+    end
+    arr
+  end
+
+  def generate_capture_down_right(board_obj)
+    arr = []
+    move_coord = @coord
+    board_obj[move_coord[0]][move_coord[1]] = nil
+    if @color
+      while in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+        move_coord = [move_coord[0] + 1, move_coord[1] + 1]
+        arr << move_coord if in_bound?(move_coord) && !board_obj[move_coord[0]][move_coord[1]].nil? && !board_obj[move_coord[0]][move_coord[1]].color
+      end
+    else
+      while in_bound?(move_coord) && board_obj[move_coord[0]][move_coord[1]].nil?
+        move_coord = [move_coord[0] + 1, move_coord[1] + 1]
+        arr << move_coord if in_bound?(move_coord) && !board_obj[move_coord[0]][move_coord[1]].nil? && board_obj[move_coord[0]][move_coord[1]].color
+      end
+    end
+    arr
+  end
 end
